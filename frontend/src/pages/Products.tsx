67c38fbd39fc3fd548e30f048ae21f843f3c6f37
AgnesFolytan <folytanagnes@gmail.com>
2024-12-11 10:55:08 +0100

    HomaPage

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,31 +1,27 @@
 import { useEffect, useState } from 'react';
 
 type Tablet = {
-  id: number;
-  nev: string;
-  ar: number;
-  marka: string;
-  operacios_rendszer: string;
-  processzor_orajel: number;
-  processzormagok_szama: number;
-  kijelzo_meret: number;
-  kijelzo_felbontasa: number;
-  ram_merete: number;
-  bemutatas: string;
+  sku: number,
+  name: String,
+  description: String,
+  price: number,
+  discount: number,
+  yarn: String
+  size: number
 };
 
-  export function ListTablets(){
-    const [tablets, setTablets] = useState<Tablet[]>([]);
+  export function ListProducts(){
+    const [products, setproducts] = useState<Tablet[]>([]);
     const [errors, setErrors] = useState<string | null>(null);
     useEffect(() => {
-        fetch("http://localhost:3000/tablets")
+        fetch("http://localhost:3000/crochetPlushies")
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
                 return response.json();
             })
-            .then((data) => setTablets(data.data))
+            .then((data) => setproducts(data.data))
             .catch((err) => setErrors(err.message));
     }, []);
 
@@ -36,25 +32,32 @@ type Tablet = {
     
   return (
     <>
-      <h2 className="mb-4">Tablet Lista</h2>
+      <h2 className="mb-4">Crochet plushies</h2>
       <div className="row">
-      {tablets.map((e) => (
-        <div key={e.id} className="col-md-4 col-lg-3 mb-4">
+      {products.map((e) => (
+        <div key={e.sku} className="col-md-4 col-lg-3 mb-4">
           <div className="card h-100">
             <div className="card-body">
-              <h5 className="card-title">{e.nev}</h5>
+              <h5 className="card-title">{e.name}</h5>
               <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                    <strong>Price:</strong> 
+                    {e.discount > 0 ? (
+                      <>
+                        <span style={{ textDecoration: 'line-through' }}>{e.price} $</span> <span>{e.discount} $</span>
+                      </>
+                    ) : (
+                      <span>{e.price} $</span>
+                    )}
+                  </li>
                 <li className="list-group-item">
-                  <strong>Ár:</strong> {e.ar} Ft
+                  <strong>Yarn:</strong> {e.yarn}
                 </li>
                 <li className="list-group-item">
-                  <strong>Márka:</strong> {e.marka}
+                  <strong>:</strong> {e.size} cm
                 </li>
                 <li className="list-group-item">
-                  <strong>Operációs rendszer:</strong> {e.operacios_rendszer}
-                </li>
-                <li className="list-group-item">
-                  <strong>Bemutatás:</strong> {e.bemutatas}
+                  <strong>Description:</strong> {e.description}
                 </li>
               </ul>
             </div>
